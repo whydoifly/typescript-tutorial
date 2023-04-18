73bd37bd40c33083fb3c631e738a3265a7ab47da
a.cheprasov <a.cheprasov@intermeda.ru>
2023-04-18 17:56:49 +0300

    Lesson 8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('testing');
+var greet;
+greet = function () {
+    console.log('hello again, world');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 20);
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,18 @@
-console.log('testing');
+let greet: Function;
 
+greet = () => {
+    console.log('hello again, world');
+}
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, 20);
+
+const minus = (a: number, b: number) => {
+    return a + b;
+}
+
+let result = minus(10, 7);
