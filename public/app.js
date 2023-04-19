94e343dab08ea739df7ddc6a0cabd01ce7e1f6f7
a.cheprasov <a.cheprasov@intermeda.ru>
2023-04-19 23:53:35 +0300

    Classes

--- public/app.js
+++ public/app.js
@@ -1,11 +1,23 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// // if (anchor) {
-// //     console.log(anchor.href);
-// // }
-// console.log(anchor.href);
+// Classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes $").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invOne.client = 'yoshi';
+invOne.amount = 666;
+console.log(invOne, invTwo);
 var form = document.querySelector('.new-item-form');
-console.log(form.children);
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
 var details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
-// const anchor = document.querySelector('a')!;
+// Classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-// // if (anchor) {
-// //     console.log(anchor.href);
-// // }
+    constructor(c: string, d:string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
 
-// console.log(anchor.href);
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+
+invOne.client = 'yoshi';
+invOne.amount = 666;
+
+console.log(invOne, invTwo);
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children)
-
 
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
