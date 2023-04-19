9006cf3e977e47c9153257e8b38661e99abd4873
a.cheprasov <a.cheprasov@intermeda.ru>
2023-04-20 00:24:44 +0300

    Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,31 @@
+// interfaces
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'shaun',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+}
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello', person.name);
+}
+
+console.log(me);
+
 import { Invoice } from './classes/Invoice.js';
+
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
